6d761c9c272ac0b4eb9aecd75d347145c589ff49
Дарья Фокина <cool.russia-33@yandex.ru>
2022-08-04 22:13:39 +0300

    initial commit

---
+++ Main1.java
@@ -0,0 +1,10 @@
+public class Main1 {
+    public static void main(String[] args) {
+        int amount = 1000;
+        int percent = 20;
+        int bonus = amount / percent;
+
+        System.out.println("здравствуйте, Вам начислены бонусы в количестве " + bonus);
+
+    }
+}
