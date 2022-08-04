f9cb70e313d12db4b334249b615c0017b6bb3035
Дарья Фокина <cool.russia-33@yandex.ru>
2022-08-04 23:46:21 +0300

    initial commit

--- Main1.java
+++ Main1.java
@@ -1,14 +1,16 @@
 public class Main1 {
     public static void main(String[] args) {
-        int amountOfMoney = 200;
-        int replenishmentAmount = 2000;
+        int eaters = 5; // сколько людей будут есть
 
-        int percent;
-        if (replenishmentAmount >= 1000) {
-            percent = replenishmentAmount / 100;
-        } else {
-            percent = 0;
-        }
-        System.out.println("Сумма ваших бонусов: " + percent);
+        int water = 3000; // миллилитров воды
+        int potatoes = 5; // картофелин
+        int chicken = 6; // куриных бёдер
+        int spices = 10; // ложек специй
+
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров воды");
+        System.out.println((potatoes / eaters) + " картофелин(а)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + " ложек(ка) специй");
     }
 }
